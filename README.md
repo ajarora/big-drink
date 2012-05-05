efee8f739e5fcae9c6bd77eefde79e006bb5fb67
AJ Arora <pitdevil@gmail.com>
2012-05-04 17:39:37 -0700

    new rails project

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Big Drink
+
+This is an app I (AJ Arora) am hacking up to track/check-off all the girly drinks I drink in San Francisco.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigDrink::Application.config.secret_token = 'eb08052585aa74979848b8e806e24751fc7a4b3a2bf16a567bbf631d389810a2fc702dc53653a08ca1d1b8088c7483968d942a305f88855026ae288dc3c819f0'
