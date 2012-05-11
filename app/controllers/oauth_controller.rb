29ebbc7ce72843c3ee46e10865fbd04631839b07
AJ Arora <pitdevil@gmail.com>
2012-05-10 21:50:26 -0700

    begin Koala integration

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,6 +1,6 @@
 class OauthController < ApplicationController
   def redirect
-    session[:access_token] = Koala::Facebook::OAuth.new("https://localhost:3000/oauth/redirect").get_access_token(params[:code]) if params[:code]
+    session[:access_token] = Koala::Facebook::OAuth.new("https://warm-snow-9881.herokuapp.com/oauth/redirect").get_access_token(params[:code]) if params[:code]
 
     redirect_to session[:access_token] ? success_path : failure_path
   end
