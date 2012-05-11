0ab97c570ed4d44270b6eb406e87c4c007041b6d
AJ Arora <pitdevil@gmail.com>
2012-05-10 23:46:01 -0700

    get FB Oauth & GraphAPI connection to work. Fuck yeah bitch.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'will_paginate', '3.0.3'
 gem 'bootstrap-will_paginate', '0.0.5'
 gem 'koala'
 gem 'friendly_id'
+gem 'thin'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -4,7 +4,7 @@ class OauthController < ApplicationController
   def redirect
     session[:access_token] = Koala::Facebook::OAuth.new(oauth_redirect_url).get_access_token(params[:code]) if params[:code]
 
-    redirect_to session[:access_token] ? success_path : failure_path
+    redirect_to session[:access_token] ? root_path : failure_path
   end
 
 end
