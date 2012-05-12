d060a06e8c782a0e00da8870c98dcfa04ed3451a
AJ Arora <pitdevil@gmail.com>
2012-05-11 18:54:26 -0700

    integrate og:drink actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,16 +5,13 @@ class StaticPagesController < ApplicationController
     
     # Get base API Connection
     @graph  = Koala::Facebook::GraphAPI.new(session[:access_token])
-
+    
     # Get public details of current application
     @app  =  @graph.get_object(FACEBOOK_APP_ID)
     
     if session[:access_token]
       @user    = @graph.get_object("me")
       @friends = @graph.get_connections('me', 'friends')
-      @photos  = @graph.get_connections('me', 'photos')
-      @likes   = @graph.get_connections('me', 'likes').first(4)
-
     end
   end  
 end
