70bd7f4d54e08e4ecf92bb613d70bc0256bd3708
AJ Arora <pitdevil@gmail.com>
2012-06-17 14:04:14 -0400

    add fog to production gem set

--- Gemfile
+++ Gemfile
@@ -35,4 +35,5 @@ end
 
 group :production do
   gem 'pg', '0.12.2'
+  gem 'fog'
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,18 +3,5 @@ class StaticPagesController < ApplicationController
   def home
     @drinks = Drink.paginate(page: params[:page])
     @json = Venue.all.to_gmaps4rails
-    
-    if current_user
-      # Get base API Connection
-      @graph  ||= Koala::Facebook::GraphAPI.new(current_user.fb_access_token)
-
-      # Get public details of current application
-      @app  ||=  @graph.get_object(FACEBOOK_APP_ID)
-      
-      @fb_user    ||= @graph.get_object("me")
-      @friends ||= @graph.get_connections('me', 'friends')
-      @user ||= User.find_by_fb_uid(@fb_user['id'])
-    end
-    
   end  
 end
