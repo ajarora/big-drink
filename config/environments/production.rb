1857778cebcd60eaa78ac665e32efcc022a4b472
AJ Arora <pitdevil@gmail.com>
2012-06-22 16:36:41 -0700

    explicitly defined default_url_options :host parameter in envornments/dev & prod.rb files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 BigDrink::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  Rails.application.routes.default_url_options[:host]= 'warm-snow-9881.herokuapp.com'
 
   # Code is not reloaded between requests
   config.cache_classes = true
