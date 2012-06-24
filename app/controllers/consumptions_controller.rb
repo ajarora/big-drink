cb385da3293e32d3662b6af0d979014ff6b4450d
AJ Arora <pitdevil@gmail.com>
2012-06-23 23:12:31 -0700

    replace drink button behavior with inline javascript (temporary stop-gap)

--- app/controllers/consumptions_controller.rb
+++ app/controllers/consumptions_controller.rb
@@ -3,10 +3,6 @@ class ConsumptionsController < ApplicationController
   def create
     @drink = Drink.find(params[:consumption][:drank_id])
     @drink.drink!(current_user)
-    respond_to do |format|
-      format.html
-      format.js
-    end
     @graph = Koala::Facebook::GraphAPI.new(current_user.fb_access_token)
     @graph.put_connections("me", "#{FB_NAMESPACE}:drink", :drink => Rails.application.routes.url_helpers.drink_url(@drink))
   end
